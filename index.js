1fb1e8398e1f531ca8ec082ac2d05b5828306e87
KKD98 <KajolKona98@gmail.com>
2023-06-21 13:12:07 +0600

    allusers rote created

--- index.js
+++ index.js
@@ -26,6 +26,15 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const usersCollection = client.db("skidsDb").collection("users");
+
+    app.get('/allusers', async(req, res) => {
+        const result = await usersCollection.find().toArray();
+        res.send(result);
+    })
+    
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
